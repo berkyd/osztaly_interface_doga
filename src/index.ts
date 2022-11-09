01c47fcd674c1362aad3654d199644786b084179
berkyd <berky.daniel@petrik.hu>
2022-11-09 13:07:03 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+let title=/[a-zA-Z ,  ]{1}/;
+let year=/[]/;
 interface ArtWork{
     title:string;
     year:Date;
     price:number;
+    height:number;
 }
 let ArtWork:ArtWork[]=[
     
 ]
+document.getElementById("gomb")?.addEventListener("click", ()=>{
+
+})
